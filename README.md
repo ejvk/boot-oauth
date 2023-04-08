65f606c66b21f0936be5f34d053f3e61c778eebc
ejvk <ejvk@nothing.com>
2023-04-08 09:56:55 -0400

    cleanup

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 * Copy private.properties.template as private.properties
 * Fill in the properties in private.properties
 * Start the application
+
+## Notes
+* /login - built in Spring login page
+* /logout - built in Spring logout page
+* Demo provides login options for Google, Github, and Okta
--- src/main/java/com/config/AppConfig.java
+++ src/main/java/com/config/AppConfig.java
@@ -3,10 +3,6 @@ package com.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,20 +14,12 @@ public class AppConfig {
             .anyRequest()
             .authenticated()
             .and()
-            .oauth2Login();
+            .oauth2Login()
+            .and()
+            .logout()
+            .logoutSuccessUrl("/login")
+            .deleteCookies("JSESSIONID");
         return http.build();
     }
-
-//    @Bean
-//    public ClientRegistrationRepository clientRepository() {
-//        ClientRegistration clientReg = clientRegistration();
-//        return new InMemoryClientRegistrationRepository(clientReg);
-//    }
-//
-//    
-//    private ClientRegistration clientRegistration() {
-//		return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("422f316e2d76bda57798")
-//	           .clientSecret("a666b59f765695cd0ee91d209fac46219bdd388a").build();
-//	 }
 
 }
